5b9184cc31685d8b87412e2b9af3182c5ea7e47a
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-09-04 15:56:17 +0200

    ajout de la fonctionnalité de remplacement

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : remplacer;debut;fin;chaine");
+            return;
+        }
+        String texte;
+        if (parameters.length == 3) texte = "";
+        else texte = parameters[3];
+        this.document.remplacer(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]), texte);
+        super.executer();
+    }
+
+}
