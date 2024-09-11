5520229da76ab4f51a2e503461ac6d57681e561e
Lopes Manon <manon.lopes@etu.umontpellier.fr>
2024-09-11 20:45:48 +0200

    feat: commande minuscules

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -22,6 +22,7 @@ public class CommandeFactory {
             case "remplacer" : return new CommandeRemplacer(document, parameters);
             case "majuscules" : return new CommandeMajuscules(document, parameters);
             case "clear" : return new CommandeClear(document, parameters);
+            case "minuscules" : return new CommandeMinuscules(document, parameters);
             default: return null;
         }
     }
